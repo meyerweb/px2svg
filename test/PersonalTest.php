78396ee651c000951fdac41967c5251f200ed515
Kyle Brumm <kjbrum@msn.com>
2015-06-20 10:58:46 -0500

    Remove personal test

--- test/PersonalTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Px2svg\Converter;
-
-require_once('../src/Converter.php');
-
-$converter = new Converter();
-
-$output = $converter->loadImage('gmail-bozo-tag.gif')->setThreshold(10)->saveSVG('./test.svg');
