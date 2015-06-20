ac3ba2b3f0cc333a92f48ddb0a08494ebc58a593
Kyle Brumm <kjbrum@msn.com>
2015-06-20 10:31:01 -0500

    Add back other content

--- src/Converter.php
+++ src/Converter.php
@@ -223,9 +223,14 @@ class Converter
         $imp = new DOMImplementation();
         $dom = $imp->createDocument(
             null,
-            'svg'
+            'svg',
+            $imp->createDocumentType(
+                'svg',
+                '-//W3C//DTD SVG 1.1//EN',
+                'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'
+            )
         );
-        // $dom->encoding     = 'UTF-8';
+        $dom->encoding     = 'UTF-8';
         $dom->formatOutput = true;
         $dom->documentElement->setAttribute('xmlns', 'http://www.w3.org/2000/svg');
         $dom->documentElement->setAttribute('shape-rendering', 'crispEdges');
